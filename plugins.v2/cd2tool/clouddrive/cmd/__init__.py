09c64351462ee70074349d87605987a9e56f34c1
Leon <707788711@qqcom>
2025-09-14 02:10:38 +0800

    update

--- plugins.v2/cd2tool/clouddrive/cmd/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# encoding: utf-8
-
-__all__ = ["parser", "subparsers"]
-
-from .init import parser, subparsers
-
-from .iterdir import *
-from .fuse import *
--- plugins.v2/cd2tool/clouddrive/pb2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# encoding: utf-8
-
-from .proto.CloudDrive_pb2 import *
-
-if False:
-    def __getattr__(name):
-        ...
